ff15f3009073a385455df797b927ac2850c1e8f5
Matheus Mendes <matheusmendesc.16@gmail.com>
2024-07-08 21:52:45 -0300

    implments initial config security

---
+++ src/main/java/com/devsuperior/demo/config/ConfigSecurity.java
@@ -0,0 +1,17 @@
+package com.devsuperior.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ConfigSecurity {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+}
